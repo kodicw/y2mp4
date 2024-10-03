ce7f3821513f57898123d4e55971289a2be34a9d
kodicw <kodicw@gmail.com>
2024-10-03 14:03:41 -0700

    fix pytube issue https://github.com/pytube/pytube/issues/1973 and add Werkzeug==2.2.2 to requirements.txt

--- app.py
+++ app.py
@@ -1,33 +1,43 @@
 from flask import Flask, request, send_file, redirect
 import pyvibe as pv
-from ytmp3 import YouTube, download_video
+from ytmp3 import download_video
 from urllib.parse import unquote
+import fixpytube
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    page = pv.Page(title="Youtube to mp4", 
-                   navbar= pv.Navbar(title="", components=[], logo="https://www.youtube.com/s/desktop/339bae71/img/favicon_144x144.png"),
-                   footer= pv.Footer(title="", components=[], logo="" ),)
+    page = pv.Page(
+        title="Youtube to mp4",
+        navbar=pv.Navbar(
+            title="",
+            components=[],
+            logo="https://www.youtube.com/s/desktop/339bae71/img/favicon_144x144.png",
+        ),
+        footer=pv.Footer(title="", components=[], logo=""),
+    )
 
     with page.add_card() as card:
         card.add_header("Youtube to mp4")
-        with card.add_form(action="/", method='POST') as form:
-            form.add_formtext(name="url", label="Video URL", placeholder="Enter the URL of the video")
+        with card.add_form(action="/", method="POST") as form:
+            form.add_formtext(
+                name="url", label="Video URL", placeholder="Enter the URL of the video"
+            )
             form.add_formsubmit(label="Download")
-    if request.method == 'POST':
-        if request.form['url']:
-            url = unquote(str(request.form['url']))
+    if request.method == "POST":
+        if request.form["url"]:
+            url = unquote(str(request.form["url"]))
             mp4 = download_video(url)
-            if mp4 == None:
-                return redirect('/')
+            if mp4 is None:
+                return redirect("/")
             print(mp4)
             page.add_card().add_header("Downloaded")
 
-            return send_file(f'{mp4}', as_attachment=True)
+            return send_file(f"{mp4}", as_attachment=True)
     return page.to_html()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- ytmp3.py
+++ ytmp3.py
@@ -3,19 +3,19 @@ import re
 
 
 def download_video(url):
-
     p = re.compile("https://youtu.be/*")
     o = re.compile("https://www.youtube.com/*")
-    if p.match(url) or o.match(url):
+    short = re.compile("https://www.youtu.be/*")
+    if p.match(url) or o.match(url) or short.match(url):
         yt = YouTube(url)
         title = "sample.mp4"
         yt.streams.get_by_itag(140).download(filename=title)
         return title
     else:
-        print('Invalid URL')
+        print("Invalid URL")
         return None
 
 
-if __name__ == '__main__':
-    url = input('Enter the URL of the video: ')
+if __name__ == "__main__":
+    url = input("Enter the URL of the video: ")
     download_video(url)
